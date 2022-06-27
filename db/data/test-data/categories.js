6f3ce08ce382fae19da068ddbf4a79cb02f3de7d
AdamG63 <adamghumra43@gmail.com>
2022-06-27 14:44:25 +0100

    add the data

---
+++ db/data/test-data/categories.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    category_name: "Power tools",
+  },
+  {
+    category_name: "Garden equipment",
+  },
+  {
+    category_name: "Electronics",
+  },
+  {
+    category_name: "Sporting goods",
+  },
+  {
+    category_name: "Musical instruments",
+  },
+  {
+    category_name: "Vehicles",
+  },
+  {
+    category_name: "Toys and games",
+  },
+];
---
+++ db/data/test-data/items.js
@@ -0,0 +1,87 @@
+module.exports = [
+  {
+    name: "Drill",
+    price: 2000,
+    body: "Powerful cordless combi drill with 2 mechanical gears and all-metal gear construction to ensure high transmission durability. Variable speed trigger covers a wide range of drilling, driving and hammer applications. Other features include 16 adjustable torque settings plus drill mode, a single sleeve keyless chuck to enable easy bit removal and a soft-grip ergonomic handle. Supplied in a moulded plastic case.",
+    user_id: 1,
+    category_id: 1,
+    item_image:
+      "https://media.istockphoto.com/photos/cordless-power-drill-picture-id184290460?k=20&m=184290460&s=612x612&w=0&h=2SPZtNwffX2rudiiftl0UZzdE5ksoqhGeR7yzIJFDaM=",
+    created_at: new Date(),
+    is_available: true,
+    location: "E1 7AE",
+  },
+  {
+    name: "Lawn mower",
+    price: 3500,
+    body: "Powered by a STIGA ST140 OHV Autochoke 139cc engine. Easy-to-use mower with a 46cm cutting width, 6 cutting heights and a mulching plug. Clippings are collected into a large 60Ltr grass bag. An integrated front bumper handle and camlock adjusters make moving or folding the handles for storage as quick and easy as possible. Suitable for lawns up to 1500m².",
+    user_id: 2,
+    category_id: 2,
+    item_image:
+      "https://media.istockphoto.com/photos/man-using-a-lawn-mower-in-his-back-yard-picture-id1096126898?s=612x612",
+    created_at: new Date(),
+    is_available: true,
+    location: "E1 7AF",
+  },
+
+  {
+    name: "Apple Macbook Pro",
+    price: 20000,
+    body: "The new M2 chip makes the 13‑inch MacBook Pro more capable than ever. The same compact design supports up to 20 hours of battery life1 and an active cooling system to sustain enhanced performance. Featuring a brilliant Retina display, a FaceTime HD camera and studio‑quality mics, it’s our most portable pro laptop.",
+    user_id: 3,
+    category_id: 3,
+    item_image:
+      "https://media.istockphoto.com/photos/computer-screen-showing-apple-event-logo-2020-on-the-screen-picture-id1283765199?s=612x612",
+    created_at: new Date(),
+    is_available: true,
+    location: "E1 7AW",
+  },
+  {
+    name: "Cricket Bat",
+    price: 3000,
+    body: "Carefully selected from Premium players grade willow",
+    user_id: 4,
+    category_id: 4,
+    item_image:
+      "https://media.istockphoto.com/photos/wooden-cricket-bat-and-ball-on-a-white-background-picture-id505125296?s=612x612",
+    created_at: new Date(),
+    is_available: true,
+    location: "E1 7AX",
+  },
+  {
+    name: "Violin",
+    price: 2000,
+    body: "Constructed from high quality tone woods. Easily change hand positions thanks to smooth fingerboard",
+    user_id: 5,
+    category_id: 5,
+    item_image:
+      "https://media.istockphoto.com/photos/single-wooden-cello-on-a-white-background-picture-id95655137?s=612x612",
+    created_at: new Date(),
+    is_available: true,
+    location: "E1 7BH",
+  },
+  {
+    name: "Car",
+    price: 20000,
+    body: "Stylish and fast family car",
+    user_id: 6,
+    category_id: 6,
+    item_image:
+      "https://media.istockphoto.com/photos/volkswagen-polo-gti-plus-in-flash-red-paint-picture-id1227424903?s=612x612",
+    created_at: new Date(),
+    is_available: true,
+    location: "E1 7BH",
+  },
+  {
+    name: "Water gun",
+    price: 1500,
+    body: "an absurdly powerful high-tech beast of a water gun that will change your game.",
+    user_id: 7,
+    category_id: 7,
+    item_image:
+      "https://media.istockphoto.com/photos/water-fight-with-my-dad-picture-id1190048038?k=20&m=1190048038&s=612x612&w=0&h=Z6FkNEj941Y1tOqD0SwSQo6HgRDTKQ5eMthRVL0EGd8=",
+    created_at: new Date(),
+    is_available: true,
+    location: "E1 7BS",
+  },
+];
