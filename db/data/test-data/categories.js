491eb4d3342f440f599ffd4fa155346980c4fbaf
AdamG63 <adamghumra43@gmail.com>
2022-06-27 15:40:06 +0100

    change the category name

--- db/data/test-data/categories.js
+++ db/data/test-data/categories.js
@@ -1,23 +1,23 @@
 module.exports = [
   {
-    category_name: "Power tools",
+    category: "Power tools",
   },
   {
-    category_name: "Garden equipment",
+    category: "Garden equipment",
   },
   {
-    category_name: "Electronics",
+    category: "Electronics",
   },
   {
-    category_name: "Sporting goods",
+    category: "Sporting goods",
   },
   {
-    category_name: "Musical instruments",
+    category: "Musical instruments",
   },
   {
-    category_name: "Vehicles",
+    category: "Vehicles",
   },
   {
-    category_name: "Toys and games",
+    category: "Toys and games",
   },
 ];
