7e393b823c9f23b65f71515dde6829b42af817b4
ffm-id <ferialfahri@gmail.com>
2022-06-27 18:11:25 +0100

    add index to exports tables data

---
+++ db/data/test-data/index.js
@@ -0,0 +1,5 @@
+exports.categoryData = require("./categories.js");
+exports.commentData = require("./comments.js");
+exports.itemData = require("./items.js");
+exports.userData = require("./users.js");
+exports.favouriteData = require("./favourites.js");
