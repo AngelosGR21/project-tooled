2a0ad3869d686061b0ab5cfa1e0c8e49b074e6d6
AngelosGR21 <angelgrigoriu@gmail.com>
2022-06-28 15:44:53 +0100

    last update in connection

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -6,11 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = require("pg");
 const ENV = process.env.NODE_ENV || "development";
 const dotenv_1 = __importDefault(require("dotenv"));
-console.log(ENV);
 dotenv_1.default.config({
     path: `${__dirname}/../../.env.${ENV}`,
 });
-console.log(`${__dirname}/../../.env.${ENV}`);
 if (!process.env.PGDATABASE) {
     throw new Error("PGDATABASE not set");
 }
