cbdb47ec8664068da741ca61618e4ba57e69cee2
ffm-id <ferialfahri@gmail.com>
2022-06-29 14:23:59 +0100

    add interface for Item

---
+++ src/__test__/types-test.ts
@@ -0,0 +1,14 @@
+export interface Item {
+    item_id: number;
+    name: string;
+    price: number;
+    body: string;
+    user_id: number;
+    category_id: number;
+    item_image: string;
+    created_at: Date;
+    is_available: boolean;
+    rating: number;
+    lat: number;
+    long: number;
+  };
