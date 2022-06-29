10fc41040a9057401672d98655af9099dfde8af4
Cadnaan <c.adnaan-afrah@hotmail.com>
2022-06-29 13:34:54 +0100

    add types

--- src/__test__/types-test.ts
+++ src/__test__/types-test.ts
@@ -2,3 +2,21 @@ export interface Category {
   category: string;
   category_id: number;
 }
+
+export interface Category {
+  category: string;
+  category_id: number;
+}
+
+export interface Item {
+  name: string;
+  price: number;
+  body: string;
+  user_id: number;
+  category_id: number;
+  item_image: string;
+  created_at: Date;
+  is_available: boolean;
+  lat: number;
+  long: number;
+}
