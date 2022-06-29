69736bf3cfdc8c44fe7a4f5cd628454c5d114cdb
Cadnaan <c.adnaan-afrah@hotmail.com>
2022-06-29 11:49:20 +0100

    update app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { Application } from "express";
 import apiRouter from "./routers/api.router";
+import categoriesRouter from "./routers/categories.router";
 const PORT = 5000;
 const app: Application = express();
 
 app.use("/api", apiRouter);
+app.use("/api/categories", categoriesRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}...`);
