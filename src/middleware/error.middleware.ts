1d61c1d9abc038693cc8d082b61847d594e84d4a
Cadnaan <c.adnaan-afrah@hotmail.com>
2022-06-29 14:57:44 +0100

    update error handle dir

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -12,13 +12,10 @@ export const handleCustomError: ErrorRequestHandler = (err, req, res, next) => {
   if (err.status) {
     res.status(err.status).send({ message: err.message });
   } else {
-    console.log(err);
-
     next(err);
   }
 };
 
 export const handleServerError: ErrorRequestHandler = (err, req, res, next) => {
   res.status(500).json({ message: "internal server error" });
-  console.log(err);
 };
