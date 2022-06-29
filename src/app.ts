d98dbee7d8b731f5c5babded6311d4fab78d026d
ffm-id <ferialfahri@gmail.com>
2022-06-29 14:23:29 +0100

    add router for /api/items

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express, { Application } from "express";
 import apiRouter from "./routers/api.router";
+import itemsRouter from "./routers/items.router";
 const PORT = 5000;
+
 const app: Application = express();
+app.use(express.json());
 
 app.use("/api", apiRouter);
+app.use("/api/items", itemsRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}...`);
