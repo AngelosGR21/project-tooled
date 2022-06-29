b2c29d5c4e3bec1653557afcf7ad627eb8490e42
Cadnaan <c.adnaan-afrah@hotmail.com>
2022-06-29 11:49:33 +0100

    add contollers

---
+++ src/controllers/categories.controllers.ts
@@ -0,0 +1,15 @@
+import { fetchAllCategories } from "../models/categories.models";
+import { Request, Response, NextFunction } from "express";
+import { Category } from "../__test__/types-test";
+
+export const getAllCategories = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  fetchAllCategories()
+    .then((categories: Category[]) => {
+      res.status(200).send({ categories });
+    })
+    .catch(next);
+};
