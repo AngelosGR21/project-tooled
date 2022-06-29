66974af14a23f25a70e2a51ac7027d2e0c8d4ed0
ffm-id <ferialfahri@gmail.com>
2022-06-29 15:38:01 +0100

    add test in case the order passed is invalid

--- src/__test__/items.test.ts
+++ src/__test__/items.test.ts
@@ -53,4 +53,33 @@ describe('GET /api/items', () => {
             })
     });
 
+    test('200: responds with an items array of items objects which sorted by any valid column and default descending order when passed invalid order', () => {
+        return request(app)
+            .get("/api/items?sort_by=price&order=random")
+            .expect(200)
+            .then(({ body: { items } }) => {
+                expect(items).toBeInstanceOf(Array);
+                expect(items).not.toHaveLength(0);
+                expect(items).toBeSortedBy("created_at", { descending: true });
+                items.forEach((item: Item) => {
+                    expect(item).toEqual(
+                        expect.objectContaining({
+                            item_id: expect.any(Number),
+                            name: expect.any(String),
+                            price: expect.any(Number),
+                            body: expect.any(String),
+                            user_id: expect.any(Number),
+                            category_id: expect.any(Number),
+                            item_image: expect.any(String),
+                            created_at: expect.any(String),
+                            is_available: expect.any(Boolean),
+                            rating: expect.any(Number),
+                            lat: expect.any(String),
+                            long: expect.any(String)
+                        })
+                    )
+                })
+            })
+    });
+
 });
