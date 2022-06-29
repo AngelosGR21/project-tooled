711606590e13c9aafdbd1ef567352e0b301ca152
Cadnaan <c.adnaan-afrah@hotmail.com>
2022-06-29 12:32:49 +0100

    update app

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
+import { internalServerErrorHandler } from "./middleware/error.middleware";
 import apiRouter from "./routers/api.router";
 import categoriesRouter from "./routers/categories.router";
 const PORT = 5000;
@@ -7,9 +8,10 @@ const app: Application = express();
 app.use("/api", apiRouter);
 app.use("/api/categories", categoriesRouter);
 
-app.use("/*", (req, res, next) => {
+app.use("/*", (req: Request, res: Response) => {
   res.status(404).send({ message: "invalid endpoint" });
 });
+app.use(internalServerErrorHandler);
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}...`);
