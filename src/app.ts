63dd9e379b754b0a0668d5852be91d4a1a8c3bff
Cadnaan <c.adnaan-afrah@hotmail.com>
2022-06-29 11:56:23 +0100

    update app with error endpoint

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,10 @@ const app: Application = express();
 app.use("/api", apiRouter);
 app.use("/api/categories", categoriesRouter);
 
+app.use("/*", (req, res, next) => {
+  res.status(404).send({ message: "invalid endpoint" });
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}...`);
 });
