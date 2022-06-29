946cd4b95df231e11a630de7dd566af1bdfee45a
ffm-id <ferialfahri@gmail.com>
2022-06-29 16:28:44 +0100

    Add test for invalid sort by

--- src/__test__/items.test.ts
+++ src/__test__/items.test.ts
@@ -83,6 +83,15 @@ describe("API: /api/items", () => {
         })
     });
 
+    test('400: responds with bad request message when passed invalid sort_by', () => {
+      return request(app)
+        .get("/api/items?sort_by=oranges")
+        .expect(400)
+        .then(({ body }) => {
+          expect(body.message).toBe("Invalid sort by");
+        });
+    });
+
   });
   describe("GET: /api/items/:item_id", () => {
     test("200: responds with a item object", () => {
