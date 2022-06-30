a112d02023eac8618c06a362abb639c922947775
AngelosGR21 <angelgrigoriu@gmail.com>
2022-06-30 12:45:23 +0100

    adding users router and error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,21 @@
 import express, { Application } from "express";
 import apiRouter from "./routers/api.router";
+import usersRouter from "./routers/users.router";
+import { handleCustomError, handleServerError } from "./middleware/error.middleware";
+
 const PORT = 5000;
 const app: Application = express();
 
+app.use(express.json());
+
+
 app.use("/api", apiRouter);
+app.use("/api/users", usersRouter);
+
+
+app.use(handleCustomError);
+app.use(handleServerError);
+
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}...`);
