07708ed9c2ff47911b4d5f63e8988fc4eedaafe4
AngelosGR21 <angelgrigoriu@gmail.com>
2022-07-03 21:26:03 +0100

    adding postAuth middleware

--- src/middleware/authorization.middleware.ts
+++ src/middleware/authorization.middleware.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import e, { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import { UserDetails } from "../types/user.types";
@@ -17,4 +17,22 @@ export const locationAuth = (req: Request, res: Response, next: NextFunction) =>
     }
 
     next()
+}
+
+export const postAuth = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (token) {
+
+        jwt.verify(token, String(process.env.TEST_KEY), (err, decoded) => {
+            if (err) {
+                next({ status: 410, message: "Token expired or is invalid, login again" })
+            }
+
+            res.locals.user = decoded;
+
+            next()
+        });
+    } else {
+        next({ status: 401, message: "you are not logged in" })
+    }
 }
